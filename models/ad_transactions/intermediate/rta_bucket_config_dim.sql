3d3360d1cd68b3643ae803dc605ba98a31d4691d
Yining <yijia.go@gmail.com>
2023-04-13 10:44:27 +0800

    fix error

--- models/ad_transactions/intermediate/rta_bucket_config_dim.sql
+++ models/ad_transactions/intermediate/rta_bucket_config_dim.sql
@@ -6,7 +6,7 @@ with rta_bucket_config_dim as (
         bucket,
         input_date,
         date_format(input_date,'yyyyMMdd')input_pday,
-        first(strategys_name) strategys_name
+        first(strategys_name) as strategys_name
     from
         (
             select 
@@ -15,7 +15,11 @@ with rta_bucket_config_dim as (
                 , input_date
                 , LATERAL VIEW EXPLODE(split(buckets, ",")) as bucket
             from (
-                select strategys_name, config_id, input_date, buckets
+                select 
+                    strategys_name
+                    , config_id
+                    , input_date
+                    , buckets
                 from {{ source('fin_dim', 'dim_m_audience_group_rta_group_config_daily_jwm_pdi') }} 
                 where input_date >=date_sub(date('{{ var("pday") }}') ,8)
                 ) temp_table_1
