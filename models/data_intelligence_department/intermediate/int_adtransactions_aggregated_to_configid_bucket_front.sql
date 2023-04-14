f5865bb18b598e18a7d5cef2ade9da900fda0c5f
Yining <yijia.go@gmail.com>
2023-04-14 18:41:05 +0800

    fix sql error

--- models/data_intelligence_department/intermediate/int_adtransactions_aggregated_to_configid_bucket_front.sql
+++ models/data_intelligence_department/intermediate/int_adtransactions_aggregated_to_configid_bucket_front.sql
@@ -14,6 +14,7 @@ adtransactions_aggregated_to_configid_bucket_front as (
         , count(distinct imp_device_md5) as imp_uv
         , sum(clk_pv) as clk_pv
         , count(distinct clk_device_md5) as clk_uv
+        , sum(imp_cpm_cost) as imp_cpm_cost
     from (
       select 
             p_day
@@ -24,6 +25,7 @@ adtransactions_aggregated_to_configid_bucket_front as (
           , clk_device_md5
           , sum(imp_agg_ct) as imp_pv
           , sum(clk_agg_ct) as clk_pv
+          , sum(imp_cpm_cost) as imp_cpm_cost
       from ad_transactions_details
       group by 1,2,3,4,5,6
     ) ad_f
