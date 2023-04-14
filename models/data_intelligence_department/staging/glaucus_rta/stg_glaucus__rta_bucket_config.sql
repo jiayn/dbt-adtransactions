fb35296d1459f901d25d4688a3b36b935ac34242
Yining <yijia.go@gmail.com>
2023-04-14 16:16:15 +0800

    fix dim sql error

--- models/data_intelligence_department/staging/glaucus_rta/stg_glaucus__rta_bucket_config.sql
+++ models/data_intelligence_department/staging/glaucus_rta/stg_glaucus__rta_bucket_config.sql
@@ -10,25 +10,14 @@ renamed as (
         config_id,
         bucket,
         input_date,
-        date_format(input_date,'yyyyMMdd') as input_pday,
-        first(strategys_name) as strategys_name
+        date_format(input_date,'yyyyMMdd')input_pday,
+        first(strategys_name) strategys_name
     from
         (
-            select 
-                strategys_name
-                , config_id
-                , input_date
-                , LATERAL VIEW EXPLODE(split(buckets, ",")) as bucket
-            from (
-                select 
-                    strategys_name
-                    , config_id
-                    , input_date
-                    , buckets
-                from source
-                where input_date >=date_sub(date('{{ var("pday") }}') ,8)
-            ) temp_table_1
-        ) temp_table_2
+            select strategys_name, config_id, input_date, bucket
+            from source LATERAL VIEW EXPLODE(split(buckets, ",")) as bucket
+            where input_date >=date_sub(date('{{ var("pday") }}') ,8)
+        )
     group by 1,2,3,4
 )
 
